d15bbe000e0a55135d1f763e395fc437cb46da40
JiaJiunn <ja497@cornell.edu>
2020-03-26 18:11:13 -0400

    nms

--- utils.py
+++ utils.py
@@ -130,7 +130,64 @@ def write_results(prediction, confidence, num_classes, nms_conf=0.4):
         # NMS class-wise
 
         for cls in img_classes:
-            pass
+
+            # for this class, get the detections
+            cls_mask = image_pred_ * \
+                (image_pred_[:, -1] == cls).float().unsqueeze(1)
+            class_mask_ind = torch.nonzero(cls_mask[:, -2]).squeeze()
+            image_pred_class = image_pred_[class_mask_ind].view(-1, 7)
+
+            # sort the detections such that the entry with the maximum objectness
+            # confidence is at the top
+            conf_sort_index = torch.sort(
+                image_pred_class[:, 4], descending=True)[1]
+            image_pred_class = image_pred_class[conf_sort_index]
+            idx = image_pred_class.size(0)
+
+            # use max obj conf detection for NMS
+            for i in range(idx):
+
+                # get IOUs of all boxes after the (max) one we are looking at in the loop
+                try:
+                    # iou of first bb compared to a tensor of multiple rows of bbs
+                    ious = bbox_iou(image_pred_class[i].unsqueeze(
+                        0), image_pred_class[i+1:])
+
+                # image_pred_class removes bb as we iterate, causing index out of bounds
+                # in that case can just exit the loop (because no further bb to remove)
+                except ValueError:
+                    break
+                except IndexError:
+                    break
+
+                # TODO understand this
+                # zero out all the detections that have IoU > treshold
+                iou_mask = (ious < nms_conf).float().unsqueeze(1)
+                image_pred_class[i+1:] *= iou_mask
+
+                # remove the zero entries
+                non_zero_ind = torch.nonzero(image_pred_class[:, 4]).squeeze()
+                image_pred_class = image_pred_class[non_zero_ind].view(-1, 7)
+
+            # repeat the batch_id for as many detections of the class cls in the image # TODO ???
+            batch_ind = image_pred_class.new(
+                image_pred_class.size(0), 1).fill_(ind)
+            seq = batch_ind, image_pred_class
+
+            # add this true detection
+            # do not init tensor unless we have detection to assign
+            if not write:
+                output = torch.cat(seq, 1)
+                write = True
+            else:
+                out = torch.cat(seq, 1)
+                output = torch.cat((output, out))
+
+    # return 0 if no detections
+    try:
+        return output
+    except:
+        return 0
 
 
 def unique(tensor):
@@ -143,3 +200,30 @@ def unique(tensor):
     tensor_res = tensor.new(unique_tensor.shape)
     tensor_res.copy_(unique_tensor)
     return tensor_res
+
+
+def bbox_iou(box1, box2):
+    """
+    Returns the IoU of two bounding boxes.
+    """
+    # get the coordinates of bounding boxes
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:, 0], box1[:, 1], box1[:, 2], box1[:, 3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
+
+    # get the coordinates of the intersection rectangle
+    inter_rect_x1 = torch.max(b1_x1, b2_x1)
+    inter_rect_y1 = torch.max(b1_y1, b2_y1)
+    inter_rect_x2 = torch.min(b1_x2, b2_x2)
+    inter_rect_y2 = torch.min(b1_y2, b2_y2)
+
+    # intersection area
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * \
+        torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+
+    # union Area
+    b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
+
+    iou = inter_area / (b1_area + b2_area - inter_area)
+
+    return iou
