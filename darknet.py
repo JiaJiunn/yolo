59a055d88837872233bfeaad1e99a7a6d85589c0
JiaJiunn <ja497@cornell.edu>
2020-03-26 11:49:50 -0400

    forward pass

--- darknet.py
+++ darknet.py
@@ -1,7 +1,11 @@
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+import cv2 as cv
 
 from config import parse_cfg, create_modules
+from utils import predict_transform
 
 
 class Darknet(nn.Module):
@@ -23,6 +27,7 @@ class Darknet(nn.Module):
         # output feature map cache from previous layers, used by route and shortcut layers
         outputs = {}
 
+        write = 0
         for i, module in enumerate(modules):
             module_type = (module["type"])
 
@@ -62,4 +67,64 @@ class Darknet(nn.Module):
             elif module_type == 'yolo':
 
                 # detection layer
-                pass
+                # NOTE we cannot init empty tensor then concat
+                # so we use this whack write=0 to check if encountered first tensor
+                anchors = self.module_list[i][0].anchors
+
+                # get input dimensions and num classes
+                inp_dim = int(self.net_info["height"])
+                num_classes = int(module["classes"])
+
+                # transform
+                x = x.data
+                x = predict_transform(x, inp_dim, anchors, num_classes, CUDA)
+
+                # initialize collector
+                if not write:
+                    detections = x
+                    write = 1
+
+                else:
+                    detections = torch.cat((detections, x), 1)
+
+            # update feature map output cache
+            outputs[i] = x
+
+        return detections
+
+
+def process_test_input(img_file):
+
+    img = cv.imread(img_file)
+
+    # resize to input dim (specified in cfg file)
+    img = cv.resize(img, (608, 608))
+
+    # TODO idu this fully
+    # BGR -> RGB | H X W C -> C X H X W
+    img_ = img[:, :, ::-1].transpose((2, 0, 1))
+
+    # add a channel at 0 (for batch) and normalise
+    img_ = img_[np.newaxis, :, :, :]/255.0
+
+    # convert to float then Variable (to perform tensor ops and compute grad)
+    img_ = torch.from_numpy(img_).float()
+    img_ = Variable(img_)
+
+    return img_
+
+
+def test_forward_pass(cfg_file, img_file):
+
+    model = Darknet(cfg_file)
+    input_img = process_test_input(img_file)
+
+    # feed into forward pass
+    pred = model(input_img, False)  # torch.cuda.is_available()
+    print(pred)
+    print(pred.size())
+    # 1 batch size
+    # 85 per row for 4 bbox attributes, 1 objectness score, and 80 class scores
+
+
+test_forward_pass('cfg/yolov3.cfg', 'imgs/dog_cycle_car.png')
--- utils.py
+++ utils.py
@@ -1,6 +1,66 @@
+import torch
+import numpy as np
+
+
 def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA=False):
     """
     [prediction] is a 3d feature map, which we convert to a 2d tensor of the 
     form: [1st bb (0,0), 2nd bb (0,0), 3rd bb (0,0), 1st bb (0,1), ,,,]
     """
-    pass
+    # set attr
+    batch_size = prediction.size(0)
+    stride = inp_dim // prediction.size(2)
+    grid_size = inp_dim // stride
+    bbox_attrs = 5 + num_classes
+    num_anchors = len(anchors)
+
+    # TODO idk what this does - copy?
+    prediction = prediction.view(
+        batch_size, bbox_attrs*num_anchors, grid_size*grid_size)
+    prediction = prediction.transpose(1, 2).contiguous()
+    prediction = prediction.view(
+        batch_size, grid_size*grid_size*num_anchors, bbox_attrs)
+
+    # divide anchors by stride
+    anchors = [(a[0]/stride, a[1]/stride) for a in anchors]
+
+    # sigmoids the centre_X, centre_Y. and object confidencce
+    prediction[:, :, 0] = torch.sigmoid(prediction[:, :, 0])
+    prediction[:, :, 1] = torch.sigmoid(prediction[:, :, 1])
+    prediction[:, :, 4] = torch.sigmoid(prediction[:, :, 4])
+
+    # TODO im not sure about this
+    # add the center offsets
+    grid = np.arange(grid_size)
+    a, b = np.meshgrid(grid, grid)
+
+    x_offset = torch.FloatTensor(a).view(-1, 1)
+    y_offset = torch.FloatTensor(b).view(-1, 1)
+
+    if CUDA:
+        x_offset = x_offset.cuda()
+        y_offset = y_offset.cuda()
+
+    x_y_offset = torch.cat((x_offset, y_offset), 1).repeat(
+        1, num_anchors).view(-1, 2).unsqueeze(0)
+
+    prediction[:, :, :2] += x_y_offset
+
+    # log space transform height and the width
+    # TODO applies anchors to bounding boxes??
+    anchors = torch.FloatTensor(anchors)
+
+    if CUDA:
+        anchors = anchors.cuda()
+
+    anchors = anchors.repeat(grid_size*grid_size, 1).unsqueeze(0)
+    prediction[:, :, 2:4] = torch.exp(prediction[:, :, 2:4])*anchors
+
+    # sigmoid activation for class scores
+    prediction[:, :, 5: 5 +
+               num_classes] = torch.sigmoid((prediction[:, :, 5: 5 + num_classes]))
+
+    # resize feature map up back to input image size
+    prediction[:, :, :4] *= stride
+
+    return prediction
